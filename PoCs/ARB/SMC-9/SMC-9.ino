495464e4ff3c1074ae05b416a989cd963c03da27
André <andre.dunkel@t-online.de>
2023-08-29 13:27:15 +0200

    SMC-9 and UnoR3 pinout added

---
+++ PoCs/ARB/SMC-9/SMC-9.ino
@@ -0,0 +1,16 @@
+/*
+   SMC-9 https://boeserdunklermann.atlassian.net/jira/software/projects/SMC/boards/2?selectedIssue=SMC-9
+   2023-08-29 --DA
+*/
+int Gpio17InputPin = 8;
+
+void setup()
+{
+  pinMode(Gpio17InputPin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, digitalRead(Gpio17InputPin));
+}
